073798e20dbb9a3906e5ca5ad967b0a5cf7b1641
QQ <cchangwo@yahoo.com.tw>
2020-11-08 16:11:47 +0800

    1. Add rest_framework view and serialize 2. add function no_of_ratings and avg_rating for movie model

--- api/views.py
+++ api/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Movie, Rating
+from .serializers import MovieSerializer, RatingSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    @action(detail=True, methods=['Post'])
+    def rate_movie(self, request, pk=None):
+        if 'stars' in request.data:
+            movie = Movie.objects.get(id=pk)
+            stars = request.data['stars']
+            user = User.objects.get(id=2)
+            print('user', user.username)
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+            except:
+                Rating.objects.create(user=user, movie=movie, stars=stars)
+
+            reponse = {'message': 'its working'}
+            return Response(reponse, status=status.HTTP_200_OK)
+        else:
+            reponse = {'message': 'not working'}
+            return Response(reponse, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = (RatingSerializer)
